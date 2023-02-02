0fd332aa58bf7f23a98422a85542b23e2f93250c
yinwenkai <yinwenkai@exiao.tech>
2023-02-02 11:35:26 +0800

    accumulation: think of angular

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 interface oType {
   username: string,
@@ -38,33 +38,75 @@ export class DemoComponent {
   //   username: 'lzj',
   //   age: 28
   // }];
-  arr: Array<any> = [{
-    className: 'class 1',
-    list: [{
-      username: 'wtw',
-      age: 29
-    }, {
-      username: 'wjj',
-      age: 29
-    }, {
-      username: 'lzj',
-      age: 28
-    }]
-  }, {
-    className: 'class 2',
-    list: [{
-      username: 'simon',
-      age: 29
-    }, {
-      username: 'frank',
-      age: 28
-    }, {
-      username: 'lily',
-      age: 26
-    }]
-  }];
+  // arr: Array<any> = [{
+  //   className: 'class 1',
+  //   list: [{
+  //     username: 'wtw',
+  //     age: 29
+  //   }, {
+  //     username: 'wjj',
+  //     age: 29
+  //   }, {
+  //     username: 'lzj',
+  //     age: 28
+  //   }]
+  // }, {
+  //   className: 'class 2',
+  //   list: [{
+  //     username: 'simon',
+  //     age: 29
+  //   }, {
+  //     username: 'frank',
+  //     age: 28
+  //   }, {
+  //     username: 'lily',
+  //     age: 26
+  //   }]
+  // }];
   // srcUrl: string = 'assets/luffy.jpeg';
-  flags:boolean = false;
+  // flags:boolean = false;
   // flags:boolean = false;
   // orderStatus: number = 3;
+  // date: Date = new Date();
+  // date: Date = new Date();
+  // title: string = '初始化数据!';
+  // setData(newTitle: string) {
+  //   this.title = newTitle;
+  // }
+  // getData() {
+  //   console.log(this.title);
+  // }
+  // onKeyDown() {
+  //   console.log('on key down');
+  // }
+  // onKeyDown(e: KeyboardEvent) {
+  // const target: EventTarget | null = e.target;
+  // const value = (target as HTMLInputElement)?.value;
+  // console.log('value:', value);
+  //   const code = e?.charCode || e?.keyCode;
+  //   const target = e?.target;
+  //   if (code === 13) {
+  //     console.log('按了一下回车!');
+  //   } else {
+  //     console.log((target as HTMLInputElement)?.value);
+  //   }
+  // }
+  // onKeyUp(e: KeyboardEvent) {
+  //   const code = e.keyCode || e.charCode;
+  //   const target = e.target;
+  //   if(code === 13) {
+  //     console.log('按了一下回车!');
+  //   } else {
+  //     console.log((target as HTMLInputElement).value);
+  //   }
+  // }
+  keyboard:string = '初始化数据!';
+
+  onGetValue() {
+    console.log(this.keyboard);
+  }
+
+  onSetValue(value: string) {
+    this.keyboard = value;
+  }
 }
